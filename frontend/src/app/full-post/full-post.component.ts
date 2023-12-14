80e057f3c82f3092414c30290cc17f121e8f60af
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-12-14 21:45:22 +0100

    [ADD] full-page-component

---
+++ frontend/src/app/full-post/full-post.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input} from '@angular/core';
+import {DateAgoPipe} from "../pipes/date-ago.pipe";
+import {Post} from "../models/post.model";
+import {PostService} from "../services/post.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-full-post',
+  standalone: true,
+  imports: [
+    DateAgoPipe
+  ],
+  templateUrl: './full-post.component.html',
+  styleUrl: './full-post.component.css'
+})
+export class FullPostComponent {
+  @Input() post!: Post
+  imageStyle!: string
+
+  constructor(protected postService: PostService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const postId = +this.route.snapshot.params['id'];
+    this.post = this.postService.getPostById(postId);
+    this.imageStyle = "url(" + this.post.imageUrl + ")"
+  }
+}
