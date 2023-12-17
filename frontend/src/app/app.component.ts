6d237ddeaa3115d07d321b313f54e26ed7543c98
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-12-17 21:06:30 +0100

    [ADD] link login and register page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,12 +9,15 @@ import {Post} from "./models/post.model";
 import {PostService} from "./services/post.service";
 import {MainPageLayoutComponent} from "./main-page-layout/main-page-layout.component";
 import {ListPostComponent} from "./list-post/list-post.component";
+import {Router} from "@angular/router";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     standalone: true,
-    imports: [CommonModule, SidebarComponent, TopbarComponent, MainPagePostComponent, ListPostComponent, MainPageLayoutComponent],
+    imports: [CommonModule, SidebarComponent, TopbarComponent, MainPagePostComponent, ListPostComponent, MainPageLayoutComponent, LoginPageComponent, RegisterPageComponent],
     styleUrls: ['./app.component.css']
 })
 
@@ -22,11 +25,16 @@ export class AppComponent implements OnInit {
     title = 'web-app';
     posts!: Post[]
 
-    constructor(private postService: PostService) {
+    constructor(private postService: PostService,
+                private router : Router) {
     }
 
     ngOnInit(): void {
         initFlowbite();
         this.posts = this.postService.getPosts()
+    }
+
+    isRoute(route: string): boolean {
+        return this.router.url === route;
     }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import {FullPostComponent} from "./full-post/full-post.component";
 import {ListPostComponent} from "./list-post/list-post.component";
 import {AccountPageComponent} from "./account-page/account-page.component";
 import {SearchPageComponent} from "./search-page/search-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
 
 export const routes: Routes = [
     {path: 'posts/:id', component: FullPostComponent},
     {path: 'account/:id', component: AccountPageComponent},
     {path: 'search/:content', component: SearchPageComponent},
-    {path: '', component: ListPostComponent}
+    {path: 'home', component: ListPostComponent},
+    {path: 'register', component: RegisterPageComponent},
+    {path: '', component: LoginPageComponent}
 ];
