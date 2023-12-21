da5e8440aa4306c8a905d36c2c1799acd8888b6c
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-12-21 15:05:37 +0100

    [ADD] link username post to account

--- frontend/src/app/main-page-post/main-page-post.component.ts
+++ frontend/src/app/main-page-post/main-page-post.component.ts
@@ -2,13 +2,14 @@ import {Component, Input} from '@angular/core';
 import {Post} from "../models/post.model";
 import {PostService} from "../services/post.service";
 import {DateAgoPipe} from "../pipes/date-ago.pipe";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-main-page-post',
   standalone: true,
   imports: [
     DateAgoPipe,
+    RouterLink,
   ],
   templateUrl: './main-page-post.component.html',
   styleUrl: './main-page-post.component.css'
