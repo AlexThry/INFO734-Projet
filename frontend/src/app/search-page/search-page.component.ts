a9e858ad034a9d2c44dfa7db57eff5c7649a6c1a
Alexis Thierry <alexist103@gmail.com>
2023-12-23 22:14:14 +0100

    [REFRACTORING]

--- frontend/src/app/search-page/search-page.component.ts
+++ frontend/src/app/search-page/search-page.component.ts
@@ -1,34 +1,28 @@
-import {Component, Input} from '@angular/core';
-import {Post} from "../models/post.model";
-import {Account} from "../models/account.model";
-import {PostService} from "../services/post.service";
-import {AccountService} from "../services/account.service";
-import {ActivatedRoute, RouterLink} from "@angular/router";
-import {DateAgoPipe} from "../pipes/date-ago.pipe";
+import { Component, Input } from "@angular/core";
+import { Post } from "../models/post.model";
+import { PostService } from "../services/post.service";
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { DateAgoPipe } from "../pipes/date-ago.pipe";
 
 @Component({
-  selector: 'app-search-page',
+  selector: "app-search-page",
   standalone: true,
-  imports: [
-    DateAgoPipe,
-    RouterLink
-  ],
-  templateUrl: './search-page.component.html',
-  styleUrl: './search-page.component.css'
+  imports: [DateAgoPipe, RouterLink],
+  templateUrl: "./search-page.component.html",
+  styleUrl: "./search-page.component.css",
 })
 export class SearchPageComponent {
-  @Input() posts!: Post[]
-  @Input() accounts!: Account[]
-  @Input() searchContent!: string
+  @Input() posts!: Post[];
+  @Input() searchContent!: string;
 
-  constructor(protected postService: PostService,
-              protected accountService: AccountService,
-              private route: ActivatedRoute) {}
+  constructor(
+    protected postService: PostService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
     // this.searchContent = this.route.snapshot.params['content'];
     // this.accounts = this.accountService.getAccountsBySearch(this.searchContent);
     // this.posts = this.postService.getPostsBySearch(this.searchContent);
   }
-
 }
