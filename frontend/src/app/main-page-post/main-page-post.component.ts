6d234c4d9e56fdd87e2f1761e72069bfa82a44a0
Alexis Thierry <alexist103@gmail.com>
2023-12-23 18:32:18 +0100

    [Feature] Adding comment on front page, changing likes display

--- frontend/src/app/main-page-post/main-page-post.component.ts
+++ frontend/src/app/main-page-post/main-page-post.component.ts
@@ -1,35 +1,45 @@
-import {Component, Input} from '@angular/core';
-import {Post} from "../models/post.model";
-import {PostService} from "../services/post.service";
-import {DateAgoPipe} from "../pipes/date-ago.pipe";
-import {Router, RouterLink} from "@angular/router";
+import { Component, Input } from "@angular/core";
+import { Post } from "../models/post.model";
+import { PostService } from "../services/post.service";
+import { DateAgoPipe } from "../pipes/date-ago.pipe";
+import { Router, RouterLink } from "@angular/router";
+import { Comment } from "../models/comment.model";
+import { CommentService } from "../services/comment.service";
+import { UserService } from "../services/user.service";
+import { User } from "../models/user.model";
 
 @Component({
-  selector: 'app-main-page-post',
+  selector: "app-main-page-post",
   standalone: true,
-  imports: [
-    DateAgoPipe,
-    RouterLink,
-  ],
-  templateUrl: './main-page-post.component.html',
-  styleUrl: './main-page-post.component.css'
+  imports: [DateAgoPipe, RouterLink],
+  templateUrl: "./main-page-post.component.html",
+  styleUrl: "./main-page-post.component.css",
 })
 export class MainPagePostComponent {
-  @Input() post!: Post
-  imageStyle!: string
-  isLike!: boolean
-  nbLike!: number
+  @Input() post!: Post;
+  imageStyle!: string;
+  isLike!: boolean;
+  nbLike!: number;
+  firstComment!: Comment;
 
-  constructor(protected postService: PostService,
-              private router : Router) {}
+  constructor(
+    protected postService: PostService,
+    private router: Router,
+    private commentService: CommentService,
+    private userService: UserService,
+  ) {}
 
   ngOnInit() {
     this.imageStyle = "url(" + this.post.image_url + ")";
     this.isLike = this.isLikeByConnectedUser();
-    this.nbLike = this.post.likes.length
-
-    // console.log(this.isLike);
-    
+    this.nbLike = this.post.likes.length;
+    try {
+      this.commentService
+        .getCommentsByPostIdLimit(this.post.id, 1)
+        .subscribe((comment) => {
+          this.firstComment = comment[0];
+        });
+    } catch {}
   }
 
   userIsLoaded() {
@@ -37,13 +47,13 @@ export class MainPagePostComponent {
   }
 
   onViewPost() {
-    this.router.navigateByUrl(`post/${this.post.id}`)
+    this.router.navigateByUrl(`post/${this.post.id}`);
   }
 
   isLikeByConnectedUser() {
     // TODO - Récupérer l'id de l'user connected
     const userConnectedID = "657c380b55f994f1b9fd2fdb";
-    
+
     return this.post.likes.includes(userConnectedID);
   }
 
@@ -54,13 +64,11 @@ export class MainPagePostComponent {
     this.isLike = !this.isLike;
 
     if (!this.isLike) {
-      this.postService.actionPostById(this.post.id, userConnectedID, 'unlike');
+      this.postService.actionPostById(this.post.id, userConnectedID, "unlike");
       this.nbLike -= 1;
-    }
-    else {
-      this.postService.actionPostById(this.post.id, userConnectedID, 'like');
+    } else {
+      this.postService.actionPostById(this.post.id, userConnectedID, "like");
       this.nbLike += 1;
     }
   }
-
 }
