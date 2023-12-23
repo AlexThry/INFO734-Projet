dcd3119391a59d3a43ee878a95b422fb05f68364
Alexis Thierry <alexist103@gmail.com>
2023-12-23 22:12:23 +0100

    [REFRACTORING]

--- frontend/src/app/list-post/list-post.component.ts
+++ frontend/src/app/list-post/list-post.component.ts
@@ -1,40 +1,39 @@
-import {Component, OnInit} from '@angular/core';
-import {MainPagePostComponent} from "../main-page-post/main-page-post.component";
-import {Post} from "../models/post.model";
-import {PostService} from "../services/post.service";
-import {NgForOf} from "@angular/common";
-import { UserService } from '../services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { MainPagePostComponent } from "../main-page-post/main-page-post.component";
+import { Post } from "../models/post.model";
+import { PostService } from "../services/post.service";
+import { NgForOf } from "@angular/common";
+import { UserService } from "../services/user.service";
 
 @Component({
-  selector: 'app-list-post',
+  selector: "app-list-post",
   standalone: true,
   imports: [MainPagePostComponent, NgForOf],
-  templateUrl: './list-post.component.html',
-  styleUrl: './list-post.component.css'
+  templateUrl: "./list-post.component.html",
+  styleUrl: "./list-post.component.css",
 })
-export class ListPostComponent implements OnInit{
+export class ListPostComponent implements OnInit {
   posts!: Post[];
   postTest!: Post;
 
-  constructor(private postService: PostService,
-              private userService: UserService) {
-  }
+  constructor(
+    private postService: PostService,
+    private userService: UserService,
+  ) {}
 
   ngOnInit() {
-    this.postService.getAllPost()
-      .subscribe(data => {
-        this.posts = data;            
-      })
+    this.postService.getAllPost().subscribe((data) => {
+      this.posts = data;
+    });
 
-      this.userService.getByIdUser('657b47c90a157aa8e0d66d87')
-        .subscribe(data => {
-          // console.log(data);
-          
-        });
+    this.userService
+      .getUserById("657b47c90a157aa8e0d66d87")
+      .subscribe((data) => {
+        // console.log(data);
+      });
   }
 
   postsAreLoaded() {
     return this.posts !== undefined;
   }
 }
-
