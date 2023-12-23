cde9d9313ade29439498454487882716e6417005
Alexis Thierry <alexist103@gmail.com>
2023-12-23 22:12:57 +0100

    [FEATURE] Adding method to get posts by user FROM/TO

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,65 +1,89 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 // CREATE
 exports.createPost = (req, res, next) => {
+  const post = new Post({
+    user_id: req.body.user_id,
+    image_url: req.body.image_url,
+    description: req.body.description,
+  });
 
-    const post = new Post({
-        user_id: req.body.user_id,
-        image_url: 'lala',
-        description: req.body.description
-    });
-  
-    post.save()
-      .then(() => res.status(201).json({ message: 'Post enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-}
+  post
+    .save()
+    .then(() => res.status(201).json({ message: "Post enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-// UPDTAE 
+// UPDTAE
 exports.updatePost = (req, res, next) => {
   Post.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Post modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-}
+    .then(() => res.status(200).json({ message: "Post modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-// ADD LIKE 
+// ADD LIKE
 exports.likePost = (req, res, next) => {
-    Post.updateOne({ _id: req.params.id }, { $addToSet: {likes: req.body.userId}, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Le user a bien été ajouté à la liste de likes du post'}))
-      .catch(error => res.status(400).json({ error }));
-}
+  Post.updateOne(
+    { _id: req.params.id },
+    { $addToSet: { likes: req.body.userId }, _id: req.params.id },
+  )
+    .then(() =>
+      res.status(200).json({
+        message: "Le user a bien été ajouté à la liste de likes du post",
+      }),
+    )
+    .catch((error) => res.status(400).json({ error }));
+};
 
-// REMOVE LIKE 
+// REMOVE LIKE
 exports.unlikePost = (req, res, next) => {
-    Post.updateOne({ _id: req.params.id }, { $pull: {likes: req.body.userId}, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Le user a bien été retiré de la liste de likes du post'}))
-      .catch(error => res.status(400).json({ error }));
-}
+  Post.updateOne(
+    { _id: req.params.id },
+    { $pull: { likes: req.body.userId }, _id: req.params.id },
+  )
+    .then(() =>
+      res.status(200).json({
+        message: "Le user a bien été retiré de la liste de likes du post",
+      }),
+    )
+    .catch((error) => res.status(400).json({ error }));
+};
 
 // DELETE
 exports.deletePost = (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Post supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-}
+  Post.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Post supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 // GET BY ID
 exports.getByIdPost = (req, res, next) => {
-    Post.findOne({ _id: req.params.id })
-    .then(post => res.status(200).json(post))
-    .catch(error => res.status(404).json({ error }));
-}
+  Post.findOne({ _id: req.params.id })
+    .then((post) => res.status(200).json(post))
+    .catch((error) => res.status(404).json({ error }));
+};
 
 // GET FROM USER ID
 exports.getFromUserIdPost = (req, res, next) => {
-    Post.find({ user_id: req.params.user_id })
-    .then(post => res.status(200).json(post))
-    .catch(error => res.status(404).json({ error }));
-}
+  Post.find({ user_id: req.params.user_id })
+    .then((post) => res.status(200).json(post))
+    .catch((error) => res.status(404).json({ error }));
+};
 
 // GET ALL
 exports.getAllPost = (req, res, next) => {
-    Post.find()
-    .then(post => res.status(200).json(post))
-    .catch(error => res.status(400).json({ error }));
-}
+  Post.find()
+    .sort({ timestamp: -1 })
+    .then((posts) => res.status(200).json(posts))
+    .catch((error) => res.status(400).json({ error }));
+};
 
+// GET POSTS BY USER ID FROM => TO
+exports.getPostsByUserIdFromLimit = (req, res, next) => {
+  Post.find({ user_id: req.params.user_id })
+    .sort({ timestamp: -1 })
+    .skip(req.params.start)
+    .limit(req.params.end)
+    .then((posts) => res.status(200).json(posts))
+    .catch((error) => res.status(400).json({ error }));
+};
