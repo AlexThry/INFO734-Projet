c47c16e5ebd547f4fde2997bfb9ee0a58923fe8f
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-12-29 14:21:56 +0100

    [FIX] reload search-page

--- frontend/src/app/search-page/search-page.component.ts
+++ frontend/src/app/search-page/search-page.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Post} from "../models/post.model";
 import {PostService} from "../services/post.service";
-import {ActivatedRoute, RouterLink} from "@angular/router";
+import {ActivatedRoute, NavigationEnd, Router, RouterLink} from "@angular/router";
 import {DateAgoPipe} from "../pipes/date-ago.pipe";
 import {UserService} from "../services/user.service";
 import {User} from "../models/user.model";
@@ -25,7 +25,8 @@ export class SearchPageComponent {
 
   constructor(protected postService: PostService,
               protected userService: UserService,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private router : Router) {}
 
   ngOnInit() {
     this.posts = []
@@ -44,7 +45,28 @@ export class SearchPageComponent {
           this.posts = data;
         })
     console.log(this.posts)
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.refreshComponent()
+      }
+    });
   }
 
+  refreshComponent() {
+    this.posts = []
+    this.users = []
+    this.searchContent = this.route.snapshot.params['content'];
+    console.log(this.searchContent)
+
+    this.userService.getUsersByTerm(this.searchContent)
+        .subscribe(data => {
+          this.users = data;
+        })
+    this.postService.getPostsBySearchTerm(this.searchContent)
+        .subscribe(data => {
+          this.posts = data;
+        })
+  }
 
 }
