79ebd81fabf504904abf0c0ccb006525e6cb7ef3
Alexis Thierry <alexist103@gmail.com>
2024-01-09 16:28:45 +0100

    [FIX] Changing comment form

--- frontend/src/app/comment-list/comment-list.component.ts
+++ frontend/src/app/comment-list/comment-list.component.ts
@@ -1,11 +1,18 @@
-import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from "@angular/core";
 import { Comment } from "../models/comment.model";
 import { CommentService } from "../services/comment.service";
 import { Post } from "../models/post.model";
 import { DateAgoPipe } from "../pipes/date-ago.pipe";
-import {Router, RouterLink} from "@angular/router";
-import {FormBuilder, FormsModule, NgForm} from "@angular/forms";
-import {User} from "../models/user.model";
+import { Router, RouterLink } from "@angular/router";
+import { FormBuilder, FormsModule, NgForm } from "@angular/forms";
+import { User } from "../models/user.model";
+declare var $: any;
 
 @Component({
   selector: "app-comment-list",
@@ -20,18 +27,21 @@ export class CommentListComponent {
   @Input() post!: Post;
   @Input() userConnected!: User;
   @Output() cancelButtonClick = new EventEmitter<void>();
-  @Input() write!:boolean;
+  @Input() write!: boolean;
   contentComment!: string;
 
-  constructor(private commentService: CommentService,
-              private router: Router) {}
+  constructor(
+    private commentService: CommentService,
+    private router: Router,
+    private elementRef: ElementRef,
+  ) {}
 
   ngOnInit() {
     this.loaded = 10;
     this.commentService
       .getCommentsByPostIdFromLimit(this.post.id, 0, this.loaded)
       .subscribe((data) => {
-        console.log(data)
+        console.log(data);
         this.commentList = data;
       });
   }
@@ -49,27 +59,31 @@ export class CommentListComponent {
     return this.commentList !== undefined;
   }
 
-
   cancelButtonClicked() {
     this.cancelButtonClick.emit();
   }
 
-  onSubmitComment(f:NgForm){
-    this.contentComment = ""
+  onSubmitComment(f: NgForm) {
+    this.contentComment = "";
 
-    if (f.value.comment != ""){
+    if (f.value.comment != "") {
       this.contentComment = f.value.comment;
 
       this.loaded = 10;
 
-      this.commentService.createcomment(this.post.id.toString(), this.userConnected.id.toString(), this.contentComment)
-          .subscribe(()=>{
-            this.commentService
-                .getCommentsByPostIdFromLimit(this.post.id, 0, this.loaded)
-                .subscribe((data) => {
-                  this.commentList = data;
-                });
-          })
+      this.commentService
+        .createcomment(
+          this.post.id.toString(),
+          this.userConnected.id.toString(),
+          this.contentComment,
+        )
+        .subscribe(() => {
+          this.commentService
+            .getCommentsByPostIdFromLimit(this.post.id, 0, this.loaded)
+            .subscribe((data) => {
+              this.commentList = data;
+            });
+        });
 
       this.cancelButtonClick.emit();
     }
