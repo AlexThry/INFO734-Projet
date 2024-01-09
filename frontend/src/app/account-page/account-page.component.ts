1ce0b99e98b27eb2b5a19e11a2d7b8f4afb22e21
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-01-09 17:07:03 +0100

    [ADD] follow user

--- frontend/src/app/account-page/account-page.component.ts
+++ frontend/src/app/account-page/account-page.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, RouterLink } from "@angular/router";
 import { User } from "../models/user.model";
 import { UserService } from "../services/user.service";
 import { AccountPostListComponent } from "../account-post-list/account-post-list.component";
+import {log} from "node:util";
 
 @Component({
   selector: "app-account-page",
@@ -20,6 +21,7 @@ export class AccountPageComponent {
   nbPosts!: number;
   nbFollower!: number;
   nbFollowing!: number;
+  follow!: boolean;
 
   constructor(
     protected postService: PostService,
@@ -27,6 +29,11 @@ export class AccountPageComponent {
     private route: ActivatedRoute,
   ) {}
   ngOnInit() {
+    // console.log(this.userConnected.id)
+    this.userService.getUserById(this.route.snapshot.params["id"])
+        .subscribe((user)=>{this.follow = user.followers.includes(this.userConnected.id)})
+    // this.follow = this.followers.includes(this.userConnected.id);
+
     // Nb Post
     this.route.params.subscribe(() => {
       let id = this.route.snapshot.params["id"];
@@ -55,5 +62,23 @@ export class AccountPageComponent {
       this.user = user;
       console.log(this.user)
     });
+  }
+
+  followUser(){
+    console.log("ca marche")
+    console.log(this.userConnected.id.toString(), this.route.snapshot.params["id"])
+    this.userService.followOtherUser(this.userConnected.id.toString(), this.route.snapshot.params["id"])
+        .subscribe(
+            ([result1, result2]) => {
+              // Handle the results of both requests
+              console.log('Result of request 1:', result1);
+              console.log('Result of request 2:', result2);
+            },
+            error => {
+              // Handle errors
+              console.error('Error:', error);
+            }
+        );
+    this.follow = true;
   }
 }
